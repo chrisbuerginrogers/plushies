6e5fb74494f4cf421912106bd967eeb98dde8879
Chris <crogers@tufts.edu>
2025-12-04 20:42:14 -0500

    Jenn's requests

--- games/controller.py
+++ games/controller.py
@@ -9,27 +9,7 @@ ROW = 10
 
 import ledmatrix
 
-class Controller:
-    def __init__(self):
-        self.display = Display()
-        self.display.clear_screen()
-        self.display.add_text('1: Music')
-        self.display.add_text('2: Shake')
-        self.display.add_text('3: Hot Cold')
-        self.display.add_text('4: Jump')
-        self.display.add_text('5: Clap')
-        self.display.add_text('6: Rainbow')
-        self.display.add_text('7: Shutdown')
-        
-        self.display.last_row = None
-        self.row = 1
-
-        self.button_up = Button(10)
-        self.button_select = Button(9)
-        self.button_down = Button(8)
-        self.pot = ADC(Pin(3))
-        self.pot.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
-        
+class Control:
     def connect(self):
         def my_callback(msg, mac, rssi):
             if not ('/ping' in msg):
@@ -63,6 +43,7 @@ class Controller:
         time.sleep(0.5)
         setup = json.dumps({'topic':'/game', 'value':game})
         self.n.publish(setup)
+
 
 class Display:
     def __init__(self):
@@ -112,43 +93,64 @@ class Button:
     def close(self):
         self.button.irq = None
 
+class Controller(Control):
+    def __init__(self):
+        self.display = Display()
+        self.display.clear_screen()
+        self.display.add_text('1: Music')
+        self.display.add_text('2: Shake')
+        self.display.add_text('3: Hot Cold')
+        self.display.add_text('4: Jump')
+        self.display.add_text('5: Clap')
+        self.display.add_text('6: Rainbow')
+        self.display.add_text('7: Shutdown')
+        
+        self.display.last_row = None
+        self.row = 1
 
-controller = Controller()
+        self.button_up = Button(10)
+        self.button_select = Button(9)
+        self.button_down = Button(8)
+        self.pot = ADC(Pin(3))
+        self.pot.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
 
-def scroll():
-    return 1+10*int(((controller.pot.read() + 1)/4095 * 6))
+if __name__ == '__main__':
+    controller = Controller()
 
-old_scroll_val = scroll()
-controller.display.box_row(old_scroll_val)
-controller.connect()
+    def scroll():
+        return 1+10*int(((controller.pot.read() + 1)/4095 * 6))
 
-i = 0
-while True:
-    i += 1
-    time.sleep(0.1)
-    #if i%10 == 1: controller.ping()
-    controller.ping()
-    
-    scroll_val = scroll()
-    controller.display.box_row(scroll_val)
-    select = int(scroll_val/10)
-    if scroll_val != old_scroll_val:
-        if controller.button_select.state == 1:
-            controller.button_select.state = 0
-            controller.display.leds.display_emoji(20+select,1,1)
-            print('select ', select)
-            controller.choose(select)
-            controller.display.leds.display_color_bar(0,10)
-            time.sleep(1)
-            old_scroll_val = scroll_val
-            
-    else:
-        if controller.button_select.state == 1:
-            controller.button_select.state = 0
-            controller.display.leds.display_emoji(29,1,1)
-            print('select again ', select)
-            controller.choose(select)
-            controller.display.leds.display_color_bar(0,10)
+    old_scroll_val = scroll()
+    controller.display.box_row(old_scroll_val)
+    controller.connect()
+
+    i = 0
+    while True:
+        i += 1
+        time.sleep(0.1)
+        #if i%10 == 1: controller.ping()
+        controller.ping()
+        
+        scroll_val = scroll()
+        controller.display.box_row(scroll_val)
+        select = int(scroll_val/10)
+        if scroll_val != old_scroll_val:
+            if controller.button_select.state == 1:
+                controller.button_select.state = 0
+                controller.display.leds.display_emoji(20+select,1,1)
+                print('select ', select)
+                controller.choose(select)
+                controller.display.leds.display_color_bar(0,10)
+                time.sleep(1)
+                old_scroll_val = scroll_val
+                
+        else:
+            if controller.button_select.state == 1:
+                controller.button_select.state = 0
+                controller.display.leds.display_emoji(29,1,1)
+                print('select again ', select)
+                controller.choose(select)
+                controller.display.leds.display_color_bar(0,10)
 
 # *    emoji: Set a number from 0 to 29 for different emoji.
 # *        0    smile        10    heart           20    house
