c7228192d0ffe5513f75d4a0e1246f4710c46c8e
Chris <crogers@tufts.edu>
2025-12-04 19:41:07 -0500

    added led matrix

--- games/controller.py
+++ games/controller.py
@@ -7,6 +7,8 @@ import  utilities.now as now
 
 ROW = 10
 
+import ledmatrix
+
 class Controller:
     def __init__(self):
         self.display = Display()
@@ -66,6 +68,8 @@ class Display:
     def __init__(self):
         i2c = SoftI2C(scl = Pin(7), sda = Pin(6))
         self.display = ssd1306.SSD1306_I2C(128, 64,i2c)
+        self.leds = ledmatrix.LEDMATRIX(i2c)
+        ledmatrix.apple(self.leds)
         self.row = 1
         self.last_row = None
         
@@ -131,13 +135,31 @@ while True:
     if scroll_val != old_scroll_val:
         if controller.button_select.state == 1:
             controller.button_select.state = 0
+            controller.display.leds.display_emoji(20+select,1,1)
             print('select ', select)
             controller.choose(select)
-            time.sleep(2)
+            controller.display.leds.display_color_bar(0,10)
+            time.sleep(1)
             old_scroll_val = scroll_val
+            
     else:
         if controller.button_select.state == 1:
             controller.button_select.state = 0
+            controller.display.leds.display_emoji(29,1,1)
             print('select again ', select)
             controller.choose(select)
+            controller.display.leds.display_color_bar(0,10)
 
+# *    emoji: Set a number from 0 to 29 for different emoji.
+# *        0    smile        10    heart           20    house
+# *        1    laugh        11    small heart     21    tree
+# *        2    sad          12    broken heart    22    flower
+# *        3    mad          13    waterdrop       23    umbrella
+# *        4    angry        14    flame           24    rain
+# *        5    cry          15    creeper         25    monster
+# *        6    greedy       16    mad creeper     26    crab
+# *        7    cool         17    sword           27    duck
+# *        8    shy          18    wooden sword    28    rabbit
+# *        9    awkward      19    crystal sword   29    cat
+# *        30   up           31    down            32    left
+# *        33   right        34    smile face 3
